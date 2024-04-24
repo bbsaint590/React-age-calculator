f4054fd2a30b8cbe2c48e65e983f73e977e50b27
Ben <benblyth590@googlemail.com>
2024-04-24 16:35:50 +0100

    project setup

---
+++ src/components/Age/index.jsx
@@ -0,0 +1,9 @@
+const Heading = ({title, subtitle}) => {
+    return (
+        <>
+         <h1>{title}</h1>
+         <h3>{subtitle}</h3>
+        </>
+    )
+}
+    export default Heading
---
+++ src/components/Input/index.jsx
@@ -0,0 +1,33 @@
+const Input = () => {
+
+
+
+return (
+
+
+<h1 className="text-red-500">Hello</h1>
+
+)
+
+}
+
+export default Input
+
+
+// <label className="amount" htmlFor="amount">{input}</label>
+// <input className="amount" type="number" name="amount" />
+
+{/* <form class="flex justify-between">
+    <div>
+        <label className="block" htmlFor="dd">Day</label>
+        <input type="text" id="dd" className="border border-slate-200" />
+    </div>
+    <div>
+        <label className="block" htmlfor="mm">Month</label>
+        <input type="text" id="mm" className="border border-slate-200" />
+    </div>
+    <div>
+        <label className="block" htmlfor="yy">Year</label>
+        <input type="text" id="yy" className="border border-slate-200" />
+    </div>
+</form> */}
